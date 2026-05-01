78e09c166206dd2484d32b7fbb9a9670e7487fb4
zhao <1934109821@qq.com>
2026-05-02 00:46:03 +0800

    update

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from PyQt6.QtWidgets import QApplication
 
 import search
 
+# 关闭 Qt 无用日志
+import os
+os.environ["QT_LOGGING_RULES"] = "*=false"
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
